f73711af94c14ed7c5f10d7facc668e92e3c0845
ajeeth-b <ajeethsiva777@gmail.com>
2021-04-22 17:54:12 +0530

    refactored

--- app/config.py
+++ app/config.py
@@ -1,15 +1,8 @@
-from os import environ
-
-
 class Config(object):
     DEBUG = False
     TESTING = False
-    GOOGLE_APPLICATION_CREDENTIAL = "path\\to\\credentials.json"
 
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
-
-
-environ['GOOGLE_APPLICATION_CREDENTIALS'] = Config.GOOGLE_APPLICATION_CREDENTIAL
