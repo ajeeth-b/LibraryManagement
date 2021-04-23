75baa7f338e562133ee01d62f932de0a09d32d76
ajeeth-b <ajeethsiva777@gmail.com>
2021-04-23 17:07:44 +0530

    password hash

--- console/library/decorators.py
+++
@@ -1,7 +0,0 @@
-def enclose_with_hash(func):
-	def wrapper(*args, **kwargs):
-		print('\n'+'##'*15)
-		result = func(*args, **kwargs)
-		print('##'*15,'\n')
-		return result
-	return wrapper
--- console/run_tests.py
+++
@@ -1,3 +0,0 @@
-from test_library import *
-from test_library_objects import *
-from test_library_object_manager import  *
