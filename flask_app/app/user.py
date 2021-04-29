f8f58a923a8ee00f87894f5d0456e50748e9afc6
ajeeth-b <ajeethsiva777@gmail.com>
2021-04-29 15:05:56 +0530

    inline to external files in html

--- flask_app/app/user.py
+++ flask_app/app/user.py
@@ -4,53 +4,12 @@ from .library_manager import query_book, BookNotFound, MemberNotFound, return_bo
 
 user_blueprint = Blueprint('user', __name__)
 
-@user_blueprint.route('/')
+@user_blueprint.route('/', methods=['GET'])
 @login_required
 def index():
 	return render_template('index.html')
 
 
-
-@user_blueprint.route('/return-book', methods=['POST'])
-def user_returns_book():
-	book_id = request.form.get('book_id', None)
-	if book_id is None:
-		return redirect('/')
-
-	if type(book_id) != int:
-		if book_id.isdigit():
-			book_id = int(book_id)
-		else:
-			return redirect('/')	
-
-	try:
-		return_book(book_id, g.user['member_id'])
-	except BookNotBorrowed:
-		return redirect('/')
-	return redirect('/')
-
-
 @user_blueprint.route('/borrow-book', methods=['GET'])
 def get_available_book_list():
 	return render_template('books.html')
-
-
-@user_blueprint.route('/borrow-book', methods=['POST'])
-def user_borrows_book():
-	book_id = request.form.get('book_id', None)
-	if book_id is None:
-		return redirect('/borrow-book')
-
-	if type(book_id) != int:
-		if book_id.isdigit():
-			book_id = int(book_id)
-		else:
-			return redirect('/borrow-book')	
-
-	try:
-		borrow_book(book_id, g.user['member_id'])
-	except BookNotBorrowed:
-		return redirect('/')
-	except MemberNotFound:
-		return redirect('/login')
-	return redirect('/')
